430b13a56011ed9bc637e7adf986248d99b601f7
LLoew <l.loewald@gmail.com>
2015-08-19 12:09:44 -0400

    updated file with new code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment #2 for R Programming class - Coursera
+## Cachematrix.R file
+## 2 functions that cashe the inverse of a mtrix
 
-## Write a short comment describing this function
+## makeCacheMatrix:  This function creates a special "matix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matcache <- NULL             ##initialize matcache
+        set <- function(y) {         ## setup the matcache in working environment
+                x <<- y
+                matcache <<- NULL
+        }
+        ## get value of matrix
+        get <- function() x          
+        ## invert the matrix 
+        setmatrix <- function(inverse) matcache <<- inverse
+        ## get the inverted matrix from matcache
+        getinverse <- function() matcache
+        
+        # return the created functions to the working environment
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve:  This function computes the inverse of the special"matrix" returned by
+## makeCacheMatrix above.  If the inverse has already been calculated and the matrix has
+## not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        matcache <- x$getinverse()
+        ## return cached inverted matrix if it exists
+        ## otherwise create the matrix in the working environment
+        if(!is.null(matcache)) {
+                message("getting cached data")
+                return(matcahe)
+        }
+        ## create the matrix - does not exist already
+        mat <- x$get()
+        matcache <- solve(mat, ...)
+        x$setmatrix(matcache)
+        matcache
 }
